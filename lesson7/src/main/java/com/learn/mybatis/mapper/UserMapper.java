d420522a0ea6639f04e5e1195a95b992ce5b144f
kristen <1065200748@qq.com>
2022-02-03 21:30:33 +0800

    2022.2.3

--- lesson7/src/main/java/com/learn/mybatis/mapper/UserMapper.java
+++ lesson7/src/main/java/com/learn/mybatis/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package com.learn.mybatis.mapper;
 
+import com.learn.mybatis.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author : Kristen
  * @date : 2022/2/3
  * @description :
  */
+@Mapper
 public interface UserMapper {
-
+    int insertUser(User user);
 }
